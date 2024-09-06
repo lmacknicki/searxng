266c487755a0f98b528a61b724388ef4fc89f45c
lmacknicki <120367192+lmacknicki@users.noreply.github.com>
2024-09-06 20:01:44 +0000

    [doc] build from commit 6b522e10713ff1309b259592b0ec9fc6402ffecc

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2024.9.6+6b522e10',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
